038aadfa32e1d2a22c55bbae26eee409ace76987
Minervus00 <fayouedraogo@protonmail.com>
2022-10-19 12:26:58 +0000

    fun

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Matches capital letters in a string and prints them one the same line
+# Matches capital letters in a string and prints them in the same line
 puts ARGV[0].scan(/[A-Z]/).join
