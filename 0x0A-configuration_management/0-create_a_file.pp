5449237892b99dc8719ff99e51afdaf2713a3688
Minervus00 <fayouedraogo@protonmail.com>
2022-12-13 12:28:42 +0000

    fun

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Using Puppet, create a file in /tmp
 
 file { '/tmp/School'
-	ensure => 'file'
-	mode => '0744'
-	owner => 'www-data'
-	group => 'www-data'
+	ensure => 'file',
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
 	content => 'I love Puppet'
 }
